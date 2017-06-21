9a666560547bd4076ac17cb7c46831ecf2be0288
Doug Mahugh <dmahugh@microsoft.com>
2017-06-21 07:11:06 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # github-unwatcher
-tool to delete GitHub repo subscriptions
+GitHub provides a [web interface](https://github.com/watching) for managed watched repositories, and you can watch or un-watch a repo with a single click. But this takes a long time, if you have hundreds of repos to un-watch. That scenario can happen pretty easily if you've been an admin for some large and active GitHub organizations, and you've had the [Automatically Watch](https://github.com/settings/notifications) checkbox set.
+
+I recently started a new role at Microsoft after a few years in MS Open Tech and the Open Source Programs Office, and I wanted to reduce my repo watch list to just the repos in a couple of orgs as well as my own repos. When I saw that I had over 900 repos on my watch list, I decided it would be faster to write a little script to automate this cleanup, rather than scrolling through all of those repos manually.
+
+## Installation / Usage
+Two prerequisites need to be installed:
+
+* Python 3.x
+* pip install requests
+
+Then follow these steps:
+
+* Clone this repo into a folder
+* Create a ```credentials.txt``` that contains your GitHub username on the first line and a _personal access token_ (as available [here](https://github.com/settings/tokens)) on the second line
+* Edit ```unwatch.py``` and modify the list passed to ```remove_subscriptions()``` to include the GitHub owners (orgs and/or users) for which you want to retain subscriptions. **All other subscriptions will be deleted**.
+* Run the program with ```python unwatch.py```
+
+Here's what it looked like when I ran it for my GitHub account:
+
+![screenshot](screenshot.png)
---
+++ unwatch.py
@@ -0,0 +1,41 @@
+"""unwatch.py
+Delete subscriptions to all GitHub repo notifications that are not
+in a specified list of orgs/user accounts."""
+import requests
+from githuberino import github_allpages
+
+def remove_subscriptions(keep=None):
+    """Remove all GitHub subscriptions that are not in a list of orgs/users.
+    keep = list of orgs or users for which subscriptions should be retained
+    """
+    to_retain = [_.lower() for _ in keep] # convert to lowercase
+
+    # username and personal access token are stored in credentials.txt,
+    # with username on 1st line and PAT on 2nd line ...
+    username, pat = open('credentials.txt').read().splitlines()
+    print('Username: {0}'.format(username))
+
+    subscriptions = github_allpages('/user/subscriptions?per_page=100',
+                                    auth=(username, pat))
+    print('Total subscriptions: {0}'.format(len(subscriptions)))
+
+    session = requests.session()
+    deleted = 0
+    retained = 0
+    for subscription in subscriptions:
+        repo = subscription['name']
+        owner = subscription['owner']['login']
+        if owner.lower() in to_retain:
+            retained += 1
+        else:
+            response = session.delete('https://api.github.com/repos/' + owner + \
+                '/' + repo + '/subscription', auth=(username, pat))
+            if not response.ok:
+                print('HTTP status code {0} - {1}/{2}'. \
+                    format(response.status_code, owner, repo))
+            deleted += 1
+    print('{0} subscriptions deleted, {1} subscriptions retained'. \
+        format(deleted, retained))
+
+if __name__ == '__main__':
+    remove_subscriptions(keep=['microsoftgraph', 'officedev', 'dmahugh'])
